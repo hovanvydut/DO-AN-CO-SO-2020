256dca18e459fd3e42ff60d9ad02d531a3256bbe
hovanvydut <hovanvydut@gmail.com>
2020-05-14 23:09:11 +0700

    structure project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <graphics.h>
+#include "calculate-utility.h"
+
+// configure
+int windowWidth = 1000;
+int windowHeight = 500;
+char windowTitle[] = "DO AN CO SO - HO VAN VY";
+
+int main() {
+    getch();
+    closegraph();
+    return 0;
+}
---
+++ myConstant.h
@@ -0,0 +1,6 @@
+// FILE
+const char inputFilePath[] = "input.txt";
+const char outputFilePath[] = "output.txt";
+
+//
+const int MAX_LENGTH_ARRAY = 10000;
